d5dc28da3b8001ed568a5fd338d23ff60c994daf
Tianxin Zhen <jackiezhen151@gmail.com>
2020-07-27 21:10:30 -0700

    update sql with constraints

--- inc/sql/db.sql
+++ inc/sql/db.sql
@@ -6,17 +6,18 @@ create table users (
 	id INT AUTO_INCREMENT PRIMARY KEY,
 	first_name VARCHAR(50),
 	last_name VARCHAR(50),
-	username VARCHAR(50),
+	username VARCHAR(50) NOT NULL UNIQUE,
 	email VARCHAR(50),
 	mobile INT,	
 	address VARCHAR(255),
-	password VARCHAR(250)
+	password VARCHAR(250) NOT NULL
 ) Engine=InnoDB;
 
 create table photos (
 	id INT AUTO_INCREMENT PRIMARY KEY,
-	name VARCHAR(50),
+	name VARCHAR(50) NOT NULL,
 	file_path VARCHAR(50),
 	description VARCHAR(100),
-	FOREIGN KEY (userid) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE
+	user_id INT NOT NULL,
+	FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE ON UPDATE CASCADE
 ) Engine=InnoDB;
