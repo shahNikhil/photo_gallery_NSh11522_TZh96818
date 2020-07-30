6fdac567b2ac27f64955d9e72a3f91088f604b7f
Jz3380 <jackiezhen151@gmail.com>
2020-07-29 17:41:42 -0700

    handle non-exist user case and fix passwor_verify

--- login.php
+++ login.php
@@ -8,25 +8,37 @@ require_once("inc/Entities/Page.class.php");
 require_once("inc/Utilities/LoginManager.class.php");
 require_once("inc/Utilities/PDOAgent.class.php");
 require_once("inc/Utilities/UserDAO.class.php");
+
+$login_message = "";
 if((isset($_POST['login'])) && !empty($_POST)){
     //Initialize the DAO
     UserDAO::init();
     //get the current user
     echo $_POST['username'];
     $authUser = UserDAO::getUser("".$_POST['username']);
-
-    //verify the password with the posted data
-    if($authUser->verifyPassword($_POST['password'])){
-        //start the session
-        session_start();
-        //set the user to login
-        $_SESSION['loggedin'] = $authUser->getUsername();
-        //point header to his photo list
-        header('Location: photo_list.php');
-    }
-    else{
+    if (gettype($authUser) === "object"){
+        //verify the password with the posted data
+        if(password_verify($_POST['password'], $authUser->getPassword())){
+            //start the session
+            session_start();
+            //set the user to login
+            $_SESSION['loggedin'] = $authUser->getUsername();
+            //point header to his photo list
+            header('Location: photo_list.php');
+        }
+        else{
+            $login_message = "Incorrect password"; //TODO: do something to display it
+            echo "<p>".$login_message."</p>";
+            sleep(5);
+            header('Location: login.php');
+        }
+    }else {
+        $login_message = "User does not exist";
+        echo "<p>".$login_message."</p>";
+        sleep(5);
         header('Location: login.php');
     }
+
 }
 else{
 Page::$subTitle = "Please enter the login credentials";
